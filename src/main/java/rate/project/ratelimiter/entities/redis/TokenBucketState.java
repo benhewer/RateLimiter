7170953457cf3e4d25bf8d36cf9e7e53709be84e
Ben <benjamin.hewer24@imperial.ac.uk>
2025-12-16 00:36:19 +0000

    Feat: Created POJOs to represent redis entries

---
+++ src/main/java/rate/project/ratelimiter/entities/redis/TokenBucketState.java
@@ -0,0 +1,45 @@
+package rate.project.ratelimiter.entities.redis;
+
+import org.springframework.data.redis.core.RedisHash;
+
+/**
+ * Maps directly to a redis entry. Stores only the temporary state of a token bucket.
+ */
+@RedisHash("token_bucket_state")
+public class TokenBucketState {
+
+  private String key;
+  private long tokens;
+  private long lastRefillTime;
+
+  public TokenBucketState(String key, long tokens, long lastRefillTime) {
+    this.key = key;
+    this.tokens = tokens;
+    this.lastRefillTime = lastRefillTime;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public long getTokens() {
+    return tokens;
+  }
+
+  public void setTokens(long tokens) {
+    this.tokens = tokens;
+  }
+
+  public long getLastRefillTime() {
+    return lastRefillTime;
+  }
+
+  public void setLastRefillTime(long lastRefillTime) {
+    this.lastRefillTime = lastRefillTime;
+  }
+
+}
